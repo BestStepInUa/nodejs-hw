486db2c1ba698a415405b81b6f629d53ebffc5bb
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-06-12 08:33:16 +0300

    add hashedPassword for register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,15 @@
 import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
 
 import { User } from '../models/user.js';
 
 export const registerUser = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
   if (user) {
     throw createHttpError(400, 'Email in use');
   }
-  const newUser = await User.create(req.body);
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashedPassword });
   res.status(201).json(newUser);
 };
