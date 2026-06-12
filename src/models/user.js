91b82d450367bff7f163c1fb07a949f2fa457570
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-06-12 14:02:36 +0300

    minor fix v1

--- src/models/user.js
+++ src/models/user.js
@@ -2,8 +2,6 @@ import { Schema, model } from 'mongoose';
 
 import { handleMongooseError, setUpdateOptions } from './hooks.js';
 
-import { emailRegexp } from '../constants/auth.js';
-
 const userSchema = new Schema(
   {
     username: {
@@ -14,7 +12,6 @@ const userSchema = new Schema(
     email: {
       type: String,
       required: true,
-      match: emailRegexp,
       unique: true,
       trim: true,
     },
